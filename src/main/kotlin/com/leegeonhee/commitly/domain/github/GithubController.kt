bf7d42201391bd6828dac7fd8c199ffda0083dd5
dlrjsgml44 <leegeh1213@gmail.com>
2025-01-24 13:40:49 +0900

    feat: GPT 요청 하루에 다섯번 제한

--- src/main/kotlin/com/leegeonhee/commitly/domain/github/GithubController.kt
+++ src/main/kotlin/com/leegeonhee/commitly/domain/github/GithubController.kt
@@ -3,6 +3,7 @@ package com.leegeonhee.commitly.domain.github
 import CommitInfo
 import com.leegeonhee.commitly.gloabl.common.BaseResponse
 import com.leegeonhee.commitly.gloabl.common.annotation.GetAuthenticatedId
+import com.leegeonhee.commitly.gloabl.util.RateLimitService
 import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -10,9 +11,12 @@ import java.time.LocalDate
 
 @RestController
 @RequestMapping("/github")
-class GitHubController(private val gitHubService: GitHubService) {
+class GitHubController(
+    private val gitHubService: GitHubService,
+    private val rateLimitService: RateLimitService
+) {
 
-//    @RateLimit
+    //    @RateLimit
     @GetMapping("/commits/messages")
     fun getCommitMessages(
         @GetAuthenticatedId userId: Long,
@@ -22,12 +26,12 @@ class GitHubController(private val gitHubService: GitHubService) {
         return gitHubService.getCommitMessages(userId, targetDate)
     }
 
-//    @RateLimit
+    //    @RateLimit
     @GetMapping("/commits/{userName}/fromDB")
     fun getFromDB(
         @PathVariable userName: String,
         @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") date: String
-    )  = gitHubService.getFromDB(userName, date)
+    ) = gitHubService.getFromDB(userName, date)
 
 
     @GetMapping("/gpt/get")
@@ -38,12 +42,20 @@ class GitHubController(private val gitHubService: GitHubService) {
 //
 //
 
-//    @RateLimit
+    //    @RateLimit
     @GetMapping("/gpt/make")
     fun getMakeCommitMessages(
         @GetAuthenticatedId userId: Long,
         @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") date: String
     ): ResponseEntity<BaseResponse<String>> {
+        if (!rateLimitService.isRequestAllowed(userId)) {
+
+            return ResponseEntity.status(429).body(BaseResponse(
+                status = 401,
+                message = "당신은 무료 요금제라서 하루에 다섯번 밖에 못함",
+            ))
+
+        }
         val response = gitHubService.generateMemoirWithGpt(userId, LocalDate.parse(date))
         return ResponseEntity.ok(response)
     }
