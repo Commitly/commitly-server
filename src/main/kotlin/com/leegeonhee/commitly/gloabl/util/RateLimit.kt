db54756be601ab64ab533666cc399b32e528ab28
dlrjsgml44 <leegeh1213@gmail.com>
2025-01-26 18:55:13 +0900

    delete: 안쓰는 코드 삭제

--- src/main/kotlin/com/leegeonhee/commitly/gloabl/util/RateLimit.kt
+++
@@ -1,5 +0,0 @@
-//package com.leegeonhee.commitly.gloabl.util
-//
-//@Target(AnnotationTarget.FUNCTION)
-//@Retention(AnnotationRetention.RUNTIME)
-//annotation class RateLimit
--- src/main/kotlin/com/leegeonhee/commitly/gloabl/util/RateLimitExceededException.kt
+++
@@ -1,3 +0,0 @@
-//package com.leegeonhee.commitly.gloabl.util
-//
-//class RateLimitExceededException(message: String) : RuntimeException(message)
