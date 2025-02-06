047e8aa30870166ccabfeb56a8fb8d95ac1a32b5
dlrjsgml44 <leegeh1213@gmail.com>
2025-02-06 17:44:26 +0900

    value -> data class

--- src/main/kotlin/com/leegeonhee/commitly/domain/retocheck/model/ItIsRetoCheckThatTheKotlinModernStyleOfValueClassIWantUseThisClassAOneTimeGood.kt
+++ src/main/kotlin/com/leegeonhee/commitly/domain/retocheck/model/ItIsRetoCheckThatTheKotlinModernStyleOfValueClassIWantUseThisClassAOneTimeGood.kt
@@ -2,7 +2,6 @@ package com.leegeonhee.commitly.domain.retocheck.model
 
 import java.time.LocalDate
 
-@JvmInline
-value class ItIsRetoCheckThatTheKotlinModernStyleOfValueClassIWantUseThisClassAOneTimeGood(
+data class ItIsRetoCheckThatTheKotlinModernStyleOfValueClassIWantUseThisClassAOneTimeGood(
     val retoDate: LocalDate,
 )
