faa2263cb26d0bcd5a5e04d4d809e8837b1735b8
dlrjsgml44 <leegeh1213@gmail.com>
2025-02-06 17:39:37 +0900

    refactor: getMyDate data class -> valueClass

--- src/main/kotlin/com/leegeonhee/commitly/domain/retocheck/RetoCheckService.kt
+++ src/main/kotlin/com/leegeonhee/commitly/domain/retocheck/RetoCheckService.kt
@@ -1,17 +1,13 @@
 package com.leegeonhee.commitly.domain.retocheck
 
-import com.leegeonhee.commitly.domain.auth.domain.entity.UserEntity
 import com.leegeonhee.commitly.domain.auth.domain.repository.UserRepository
 import com.leegeonhee.commitly.domain.retocheck.entity.RetoCheckEntity
-import com.leegeonhee.commitly.domain.retocheck.model.NoUserRetoCheckDto
-import com.leegeonhee.commitly.domain.review.model.ReviewRequestBody
-import com.leegeonhee.commitly.gloabl.common.BaseResponse
+import com.leegeonhee.commitly.domain.retocheck.model.ItIsRetoCheckThatTheKotlinModernStyleOfValueClassIWantUseThisClassAOneTimeGood
 import com.leegeonhee.commitly.gloabl.exception.CustomException
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import java.time.LocalDate
-import java.time.LocalDateTime
 
 @Service
 class RetoCheckService(
@@ -27,13 +23,12 @@ class RetoCheckService(
             )
         )
 
-    fun getMyDate(id: Long): List<NoUserRetoCheckDto> =
+    fun getMyDate(id: Long): List<ItIsRetoCheckThatTheKotlinModernStyleOfValueClassIWantUseThisClassAOneTimeGood> =
         retoCheckRepository.getAllRetoCheckEntityByAuthorOrIdNull(
             userRepository.findByIdOrNull(id) ?: throw CustomException(HttpStatus.NOT_FOUND, "없다는 뜻")
         )?.map {
-            NoUserRetoCheckDto(
+            ItIsRetoCheckThatTheKotlinModernStyleOfValueClassIWantUseThisClassAOneTimeGood(
                 retoDate = it.retoDate,
-                createdAt = it.createdAt
             )
         } ?: throw CustomException(
             status = HttpStatus.NOT_FOUND,
---
+++ src/main/kotlin/com/leegeonhee/commitly/domain/retocheck/model/ItIsRetoCheckThatTheKotlinModernStyleOfValueClassIWantUseThisClassAOneTimeGood.kt
@@ -0,0 +1,8 @@
+package com.leegeonhee.commitly.domain.retocheck.model
+
+import java.time.LocalDate
+
+@JvmInline
+value class ItIsRetoCheckThatTheKotlinModernStyleOfValueClassIWantUseThisClassAOneTimeGood(
+    val retoDate: LocalDate,
+)
